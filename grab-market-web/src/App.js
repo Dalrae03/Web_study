ffa227671e974f0390ecbc78c82d7d4c56871178
Dalrae03 <www298@naver.com>
2023-01-22 18:24:30 +0900

    React로 웹 개발하기 실습

--- grab-market-web/src/App.js
+++ grab-market-web/src/App.js
@@ -1,28 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import ChildComponent from './child';
-import TimerComponent from './timer';
+import MainPageComponent from './main';
 
 function App() {
-  const text = "강의 꾸준히 듣기";
-  const sayHello = function(){
-    return <h3>파이썬 공부도 하기</h3>
-  }
-  const sayHello2 = function(){
-    alert('안녕하세요');  // 작은 팝업창을 띄우는 함수같음
-  }
   return (
-    <div>
-      <h1>방학동안 해야할 것</h1>
-      <h2>{text}</h2>
-      {sayHello()}
-      <div onClick={sayHello2}>클릭해주세요</div>
-      <TimerComponent />
-      <ChildComponent name="달" age={24}/>
-      <ChildComponent name="민수" age={25}/>
-      <ChildComponent name="철수" age={30}/>
-      <ChildComponent name="슬기" age={28}/>
-    </div>
+      <MainPageComponent />
   );
 }
 
---
+++ grab-market-web/src/main/index.js
@@ -0,0 +1,61 @@
+import './index.css';
+import axios from 'axios';
+import React from 'react';
+
+function MainPage(){
+    const [products, setProducts]= React.useState([]);  //state 사용
+    React.useEffect(  //useEffect 사용
+        function(){
+            axios.get('https://cb42e1a2-1c38-4a71-a5da-aa11e68047d4.mock.pstmn.io/products')
+        .then(function(results){
+            const products = results.data.products;
+            setProducts(products);
+        }).catch(function(error){
+            console.error('에러 발생 : ',error);
+        })
+        },[])
+        
+    return (
+        <div>
+            <div id="header">
+                <div id="header-area">
+                    <img src="images/icons/logo.png"/>
+                </div>
+            </div>
+            <div id="body">
+                <div id="banner">
+                    <img src="images/banners/banner1.png" />
+                </div>
+                <h1>판매되는 상품들</h1>
+                <div id="product-list">
+                    {
+                        products.map(function(product, index){ //js문법 map사용
+                            return(
+                                <div className='product-card'>
+                        <div>
+                            <img className='product-img' src={product.imageUrl} />
+                        </div>
+                        <div className='product-contents'>
+                            <span className='product-name'>
+                                {product.name}
+                            </span>
+                            <span className='product-price'>
+                                {product.price}원
+                            </span>
+                            <div className='product-seller'>
+                                <img className='product-avatar' src='images/icons/avatar.png' />
+                                <span>{product.seller}</span>
+                            </div>
+                        </div>
+                    </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+            <div id="footer"></div>
+        </div>
+    )
+}
+
+export default MainPage
