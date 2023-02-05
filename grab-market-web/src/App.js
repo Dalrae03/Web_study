47bff716a719554f5ea7e372d3e83c349fa8337d
Dalrae03 <www298@naver.com>
2023-02-05 17:52:55 +0900

    웹사이트에 antd구현

--- grab-market-web/src/App.js
+++ grab-market-web/src/App.js
@@ -1,18 +1,29 @@
 import './App.css';
 import MainPageComponent from './main';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Link, useHistory} from 'react-router-dom';
 import ProductPage from './product';
 import UploadPage from './upload';
+import { Button } from 'antd';
+import {DownloadOutlined} from '@ant-design/icons';
 
 function App() {
+  const history = useHistory();
   return (
     <div>
       <div id="header">
           <div id="header-area">
-            <img src="/images/icons/logo.png"/>
-              </div>
+            <Link to="/">
+              <img src="/images/icons/logo.png"/>
+            </Link>
+            <Button size='large'
+            onClick={function() {
+              history.push('/upload');
+            }}
+            icon={<DownloadOutlined />}
+            >상품업로드</Button>
+          </div>
       </div>
-      <div id="body">
+    <div id="body">
     <Switch>
       <Route exact ={true} path={"/"}>
         <MainPageComponent />
--- grab-market-web/src/product/index.js
+++ grab-market-web/src/product/index.js
@@ -1,6 +1,7 @@
 import {useParams} from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import "./index.css";
 
 function ProductPage() {
     const {id} = useParams();
